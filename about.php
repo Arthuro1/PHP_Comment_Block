a64a9f52e244285b537f66c2d03a2faba4f1574a
Arhtur <arthur.meteng@yahoo.com>
2020-06-30 17:46:36 +0200

    first commit

---
+++ about.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+include_once "header.php";
+echo "<body>";
+include_once "navbar.php";
+
+    echo "<br /> <br /> <br />
+    <div class=\"container\">
+        <div class=\"starter-template\">
+            <h1>Ueber uns</h1>
+            <p class=\"lead\">Das hier ist der Text der Ueber uns Seite.</p>
+        </div>
+    </div>";
+
+    include_once "scripts.php";
+    include_once "footer.php";
+?>
---
+++ start.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include_once "header.php";
+echo "<body>";
+include_once "navbar.php";
+
+   echo "<br />
+    <br />
+    <br />
+
+    <div class=\"container\">
+      <div class=\"starter-template\">
+        <h1>Die Startseite</h1>
+        <p class=\"lead\">Das hier ist der Text der Startseite.</p>
+      </div>
+    </div>";
+
+include_once "scripts.php";
+include_once "footer.php";
+
+  ?>
